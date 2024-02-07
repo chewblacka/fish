8fcf4cba2567664aeef35ee12f5b81d94f8504bb
chewblacka <jgarcia3788@yahoo.co.uk>
2024-02-07 08:42:33 +0000

    moved tide_mark into main config

--- config.fish
+++ config.fish
@@ -209,3 +209,17 @@ function whitespace
         echo "No Trailing space. All is good!"
     end
 end
+
+function tide_mark
+    # Function to declaratively specify Tide settings
+    # set tide_config 312212232412
+    set tide_config 312114231312
+    # tide_config holds the intended config 
+    if test $tide_config != $tide_config_
+      echo "$tide_config y" | tide configure > /dev/null 2>&1 
+      # _tide_config_ holds the previously set config. Invisible to user.
+      # Used by TIDE to check if previous differs from intended.
+      set -U tide_config_ $tide_config
+      echo "Tide settings updated to: $tide_config"
+    end
+end
