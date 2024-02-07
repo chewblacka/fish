41da56bd0ff56d0cc9394c3cae20736c2a01bd2b
chewblacka <jgarcia3788@yahoo.co.uk>
2024-02-07 09:05:31 +0000

    tide_mark

--- config.fish
+++ config.fish
@@ -210,16 +210,19 @@ function whitespace
     end
 end
 
-function tide_mark
-    # Function to declaratively specify Tide settings
-    # set tide_config 312212232412
-    set tide_config 312114231312
-    # tide_config holds the intended config 
-    if test $tide_config != $tide_config_
-      echo "$tide_config y" | tide configure > /dev/null 2>&1 
-      # _tide_config_ holds the previously set config. Invisible to user.
-      # Used by TIDE to check if previous differs from intended.
-      set -U tide_config_ $tide_config
-      echo "Tide settings updated to: $tide_config"
+function tide_mark --argument mark
+    # Function to declaratively set Tide settings
+    if test $tide_config != $mark
+        if _tide_check $mark
+            echo "$mark y" | tide configure > /dev/null 2>&1 
+            set -U tide_config $mark
+            fish_prompt
+            true
+        else
+            echo "Invalid tide_mark sequence"
+            false
+        end  
+    else
+        true
     end
 end
